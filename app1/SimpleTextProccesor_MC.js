4388e5faee17b39ccb9283c010da325b769e9bf3
Marsel <castalanoyt@gmail.com>
2024-12-01 19:59:47 +0100

    Poprawki w SimpleTextProccesor_MC.js

--- app1/SimpleTextProccesor_MC.js
+++ app1/SimpleTextProccesor_MC.js
@@ -32,9 +32,41 @@ class SimpleTextProccesor
     } ;
 }
 
-const something = new TextProccesor(' Fortnite ');
+const something = new SimpleTextProcessor(' Fortnite ');
 console.log(something.toLowerCase());
 console.log(something.deleteText());
 console.log(something.reverseText());
 console.log(something.CountWords());
+console.log(something.toUpperCase());
+class SimpleTextProcessor {
+    constructor(text) {
+        this.text = text.trim();
+    }
+
+    toUpperCase = () => {
+        return this.text.toUpperCase();
+    }
+
+    toLowerCase = () => {
+        return this.text.toLowerCase();
+    }
+    
+    reverseText = () => {
+        return this.text.split('').reverse().join('');
+    }
+    
+    deleteExtraSpaces = () => {
+        return this.text.replace(/\s+/g, ' ').trim();
+    }
+    
+    countWords = () => {
+        return this.text.split(' ').length; 
+    }
+}
+
+const something = new SimpleTextProcessor(' Fortnite ');
+console.log(something.toLowerCase());
+console.log(something.deleteExtraSpaces());
+console.log(something.reverseText());
+console.log(something.countWords());
 console.log(something.toUpperCase());
